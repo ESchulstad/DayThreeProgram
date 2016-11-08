130a6092e0ce45c4be6b46eae0bee7f79467b778
ESchulstad <eschulstad@gmail.com>
2016-11-07 21:00:51 -0500

    initial commit

---
+++ DayThreeProgram/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayThreeProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter = 'd';
+
+            switch(letter)
+            {
+                case 'a':
+
+                    Console.WriteLine("it's an a.");
+                    break;
+
+                case 'e':
+
+                    Console.WriteLine("it's an e.");
+                    break;
+
+                case 'd':
+
+                    Console.WriteLine("it's a d.");
+                    break; 
+
+
+                default:
+
+                    Console.WriteLine("unknown character");
+                    break;
+            }
+        }
+    }
+}
